b995aa5935e464cedad62a34bb1c22d5d2879635
ann-jrti <asog.jrti@gmail.com>
2021-09-15 21:26:08 +0200

    new folders for each file

--- blockchain.js
+++ blockchain.js
@@ -1,29 +1,9 @@
 const sha256 = require('crypto-js/sha256');
-
-function Block (index, creationDate, anyData, previousHash = '') {
-    this.index = index;
-    this.creationDate = creationDate;
-    this.anyData = anyData;
-    this.previousHash = previousHash;
-    this.hash = this.hashCalculator();
-    this.nonce = 0;
-}
-
-Block.prototype.hashCalculator = function () {
-    return sha256(this.index + this.creationDate + JSON.stringify(this.anyData) + this.previousHash + this.nonce).toString();
-}
-
-Block.prototype.mineBlock = function (difficulty) {
-    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
-        this.nonce++;
-        this.hash = this.hashCalculator();
-    }
-    console.log(`Block successfuly mined! ${this.hash}`);
-}
+const { Block } = require('./block.js');
 
 function Blockchain() {
-    this.chain = [this.createGenesisBlock()]; //array of blocks
-    this.difficulty = 4;
+    this.chain = [this.createGenesisBlock()]; 
+    this.difficulty = 2;
 }
 
 Blockchain.prototype.createGenesisBlock = function() {
@@ -53,19 +33,6 @@ Blockchain.prototype.validChainOrNot = function() {
 
 let whateverCoin = new Blockchain();
 
-console.log('Mining block 1...')
-whateverCoin.addBlock(new Block(1, "11/09/2021", { amount : 5, 'size': '22834 bytes' }));
-console.log('Mining block 2...')
-whateverCoin.addBlock(new Block(2, "12/09/2021", { amount : 21, 'size': '83983 bytes' }));
-console.log('Mining block 3...')
-whateverCoin.addBlock(new Block(3, "13/09/2021", { amount : 13, 'size': '56690 bytes' }));
+module.exports = { Blockchain }
+module.exports = { whateverCoin }
 
-
-console.log(JSON.stringify(whateverCoin, null, 4))
-
-console.log(`blockchain validation: ${whateverCoin.validChainOrNot()}`);
-
-whateverCoin.chain[1].anyData = { amount : 100};
-whateverCoin.chain[1].hash = whateverCoin.chain[1].hashCalculator();
-
-console.log(`blockchain validation: ${whateverCoin.validChainOrNot()}`);
